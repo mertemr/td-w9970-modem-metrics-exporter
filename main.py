2fcec79d310c4e9309e34e52bbac844414ebb50c
mertemr <48881002+mertemr@users.noreply.github.com>
2025-02-15 11:24:11 +0300

    Add HTTP Server and metrics endpoint for prometheus

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import re
 from base64 import b64encode
 from http.client import HTTPConnection
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 from dotenv import load_dotenv
 
@@ -12,6 +13,31 @@ MODEM_USERNAME = os.getenv("MODEM_USERNAME")
 MODEM_PASSWORD = os.getenv("MODEM_PASSWORD")
 
 ENDPOINT = "/cgi?1=null&5=null"
+
+SERVER_PORT = int(os.getenv("PORT", 8000))
+
+
+class MetricsHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if not self.path == "/metrics":
+            self.send_response(404)
+            self.end_headers()
+            return
+
+        try:
+            metrics = fetch_dsl_metrics()
+            prometheus_output = prometheus_format(metrics)
+        except Exception as e:
+            print(e)
+            self.send_response(500)
+            self.end_headers()
+            return
+
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(prometheus_output.encode("utf-8"))
+
 
 payload = (
     "[WAN_DSL_INTF_CFG#1,0,0,0,0,0#0,0,0,0,0,0]0,12\r\n"
@@ -28,7 +54,6 @@ payload = (
     "upstreamAttenuation\r\n"
     "downstreamAttenuation\r\n"
 )
-
 auth_b64 = b64encode(f"{MODEM_USERNAME}:{MODEM_PASSWORD}".encode()).decode()
 
 
@@ -60,5 +85,27 @@ def fetch_dsl_metrics():
     return matches
 
 
+def prometheus_format(metrics: dict[str, str]) -> str:
+    output = ""
+    output += "# HELP dsl_modem_metrics DSL metrics from the modem\n"
+    output += "# TYPE dsl_modem_metrics gauge\n"
+
+    for key, value in metrics.items():
+        key = key.strip().replace(".", "_").replace("-", "_")
+        output += f"dsl_modem_{key} {value}\n"
+
+    return output
+
+
 if __name__ == "__main__":
-    print(fetch_dsl_metrics())
+    # fetch_dsl_metrics()
+    server_address = ("127.0.0.1", SERVER_PORT)
+    httpd = HTTPServer(server_address, MetricsHandler)
+    print(f"Access the metrics at http://127.0.0.1:{SERVER_PORT}/metrics")
+
+    try:
+        httpd.serve_forever()
+    except Exception as e:
+        print(e)
+    finally:
+        httpd.server_close()
